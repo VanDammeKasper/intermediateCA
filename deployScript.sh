d9650310605d91cb34a93771ba1236006710aade
Kasper Van Damme <kasper.vandamme.w9577@student.hogent.be>
2019-03-08 10:17:09 +0100

    Update deployScript.sh

--- deployScript.sh
+++ deployScript.sh
@@ -1,33 +1,40 @@
-#! bin/bash
+#! /bin/bash
 #variablen
 
-cert_privateKey_destination="/opt/zimbra/commercial/commercail.key"
+cert_privateKey_destination="/opt/zimbra/ssl/zimbra/commercial/commercial.key"
 
 cert_domain="mail.annabeltextiles.com"
 
 
 # append fullchain
-printf -- 'appending fullchain'
+printf -- 'appending fullchain\n'
 git clone https://github.com/vandammekasper/intermediateCA/
 
+sleep 5
 
-cat fullchain.cer intermediateCA/interemediate.cer > temp.cer
+cat fullchain.cer intermediateCA/intermediate.cer > temp.cer
+
+sleep 3
 rm fullchain.cer
+
+sleep 3
 mv temp.cer fullchain.cer
-rm temp.cer
+
 
 
 # move privatekey
 
-printf -- 'moving private key'
-mv $cert_domain.cer $cert_privateKey_destination
+printf -- 'copying private key\n'
+cp $cert_domain.cer $cert_privateKey_destination
 
 # verify certs
 
-/opt/zimbra/bin/zmcertmgr verifycrt comm $cert_privateKey_destination $cert_domain.cer fullchain.cer
+
+printf -- 'verifying certificates \n'
+sudo -u zimbra /opt/zimbra/bin/zmcertmgr verifycrt comm $cert_domain.key $cert_privateKey_destination fullchain.cer
 
 
 # deploy certs
 
-printf -- 'deploying certificates'
-/opt/zimbra/bin/zmcertmgr deploycrt comm $cert_domain.cer fullchain.cer
+printf -- 'deploying certificates\n'
+sudo -u zimbra /opt/zimbra/bin/zmcertmgr deploycrt comm $cert_domain.cer fullchain.cer
