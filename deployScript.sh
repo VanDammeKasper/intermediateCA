2cfc725a3982fbe8693ec660a0f4beda6649da20
Kasper Van Damme <kasper.vandamme.w9577@student.hogent.be>
2019-03-11 14:27:28 +0100

    Update deployScript.sh

--- deployScript.sh
+++ deployScript.sh
@@ -24,16 +24,21 @@ mv temp.cer fullchain.cer
 
 # move privatekey
 
+printf -- 'removing old private key\n'
+rm $cert_privateKey_destination
+
 printf -- 'copying private key\n'
-cp $cert_domain.cer $cert_privateKey_destination
+cp $cert_domain.key $cert_privateKey_destination
+
+chown zimbra:zimbra $cert_privateKey_destination
 
 # verify certs
-
+sleep 3
 
 printf -- 'verifying certificates \n'
 sudo -u zimbra /opt/zimbra/bin/zmcertmgr verifycrt comm $cert_domain.key $cert_privateKey_destination fullchain.cer
 
-
+sleep 3
 # deploy certs
 
 printf -- 'deploying certificates\n'
